a57e5a012c70fbef4e607d0f310e5b0ade3bde46
moemi ohtsu <s0623192@gmail.com>
2020-04-08 23:37:54 +0900

    add: 単一ファイルコンポーネント（グローバル・ローカル）追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import LikeNumber from "./components/LikeNumber";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component('LikeNumber', LikeNumber); //グローバル登録
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
