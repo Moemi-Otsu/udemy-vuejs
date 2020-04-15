4a6aa28dae1c02a8861490c91360341010c08833
moemi ohtsu <s0623192@gmail.com>
2020-04-15 23:39:10 +0900

    add: v-modelの修飾子やinputの場合のdata型について

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,68 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
+    <div>
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input
+        id="title"
+        type="text"
+        v-model.lazy="eventData.title"
+      >
+      <p>{{ eventData.title }}</p>
+      <label for="maxNumber">最大人数</label>
+      <input
+              id="maxNumber"
+              type="number"
+              v-model.number="eventData.maxNumber"
+      >
+      <p>{{ eventData.maxNumber }}</p>
+      <label for="host">主催者</label>
+      <input
+              id="host"
+              type="text"
+              v-model.trim="eventData.host"
+      >
+      <p>{{ eventData.host }}</p>
+      <lavel for="detail">イベント内容</lavel>
+      <textarea
+        id="detail"
+        cols="30"
+        rows="10"
+        v-model="eventData.detail"
+      ></textarea>
+      <p style="white-space: pre;">{{ eventData.detail }}</p>
+      <input
+        type="checkbox"
+        id="isPrivate"
+        v-model="eventData.isPrivate"
+      >
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate }}</p>
+      <p>参加条件</p>
+      <input
+        type="checkbox"
+        id="10"
+        value="10代"
+        v-model="eventData.target"
+      >
+      <label for="10">10代</label>
+      <input
+        type="checkbox"
+        id="20"
+        value="20代"
+        v-model="eventData.target"
+      >
+      <label for="20">20代</label>
+      <input
+        type="checkbox"
+        id="30"
+        value="30代"
+        v-model="eventData.target"
+      >
+      <label for="30">30代</label>
+      <p>{{ eventData.target }}</p>
+    </div>
   </div>
 </template>
 
@@ -21,7 +83,15 @@ export default {
   data() {
     return {
       number: 14,
-      currentComponent: "Home"
+      currentComponent: "Home",
+      eventData: {
+        title: "",
+        maxNumber: 8,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: []
+      }
     };
   },
   components: {
@@ -36,9 +106,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  div {
-    border: 1px solid blue;
-  }
-</style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -4,14 +4,3 @@
     <input>
   </div>
 </template>
-
-<script>
-export default {
-  deactivated() {
-    console.log("deactivated");
-  },
-  activated() {
-    console.log("activated");
-  }
-}
-</script>
