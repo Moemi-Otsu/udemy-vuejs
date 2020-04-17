f931a50d3233a07d65c58b009fc1c62727cb30ac
moemi ohtsu <s0623192@gmail.com>
2020-04-17 23:54:26 +0900

    add: カスタムディレクティブ-修飾子まで

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import LikeNumber from "./components/LikeNumber";
 Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber); //グローバル登録
 Vue.directive("border", function(el, binding) {
-  el.style.border = "solid black 2px";
-  el.style.borderWidth = binding.value.width; //binding.valueでテンプレから動的にデータを受け取れる
+  el.style.borderWidth = binding.value.width;
   el.style.borderColor = binding.value.color;
+  el.style.borderStyle = binding.arg;
+  if (binding.modifiers.round) {
+    el.style.borderRadius = "0.5rem";
+  }
+  if (binding.modifiers.shadow) {
+    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+  }
 });
 
 new Vue({
